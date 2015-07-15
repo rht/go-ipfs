221ac254849ebcfddd90d19d49bafaa5d31a0c8b
Tommi Virtanen <tv@eagain.net>
2015-07-15 08:36:48 -0700

    gofmt generated assets
    The generated file went through some changes because of differing
    go-bindata versions.
    
    License: MIT
    Signed-off-by: Tommi Virtanen <tv@eagain.net>

--- pin/pin_test.go
+++ pin/pin_test.go
@@ -204,7 +204,7 @@ func TestPinRecursiveFail(t *testing.T) {
 
 	a, _ := randNode()
 	b, _ := randNode()
-	err = a.AddNodeLinkClean("child", b)
+	err := a.AddNodeLinkClean("child", b)
 	if err != nil {
 		t.Fatal(err)
 	}
